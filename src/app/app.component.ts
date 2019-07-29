e9d3ed07520d316875bd65225773475f1239cdcd
Lina María Uribe <37450729+linamariaum@users.noreply.github.com>
2019-07-29 18:42:15 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gestionCurriculo';
+  public cursos;
+  
+  constructor() {  
+    this.cursos = [
+      {
+        nombre: "INTRO A LA INGEN D SISTEM",
+        materia: "2508120",
+        nivel: "1",
+        creditos: "1",
+        hp: "0",
+        he: "0",
+        ht: "0",
+        htp: "2",
+        correquisitos: "",
+        prerrequisitos: ""
+      },
+      {
+        nombre: "ÁLGEBRA Y TRIGONOMETRÍA",
+        materia: "2555101",
+        nivel: "1",
+        creditos: "3",
+        hp: "0",
+        he: "0",
+        ht: "4",
+        htp: "0",
+        correquisitos: "",
+        prerrequisitos: ""
+      }, 
+      {
+        nombre: "MATEMÁTICAS DISCRETAS I",
+        materia: "2508207",
+        nivel: "2",
+        creditos: "3",
+        hp: "0",
+        he: "0",
+        ht: "0",
+        htp: "4",
+        correquisitos: "",
+        prerrequisitos: "2508120"
+      }, 
+    ];    
+  }
+  
+  
 }
