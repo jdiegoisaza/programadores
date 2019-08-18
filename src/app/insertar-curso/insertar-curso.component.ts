03c60d24c6d3f5eca8a8a72d5c3e2b3e8478294c
Daniel <daniel.jaramillo7@udea.edu.co>
2019-08-17 21:31:59 -0500

    Componente para insertar curso

---
+++ src/app/insertar-curso/insertar-curso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insertar-curso',
+  templateUrl: './insertar-curso.component.html',
+  styleUrls: ['./insertar-curso.component.css']
+})
+export class InsertarCursoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
