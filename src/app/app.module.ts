c4acce8c2411c365e6d4bb7a4127408ba04b7aac
Jorge <andreshiler@gmail.com>
2019-08-19 12:39:32 -0500

    Home
    Pagina de inicio sin direccionamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule, Router } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './homeComponents/home/home.component';
+import { NavbarComponent } from './homeComponents/home/navbar/navbar.component';
+import { ContentComponent } from './homeComponents/home/content/content.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
@@ -14,3 +27,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
